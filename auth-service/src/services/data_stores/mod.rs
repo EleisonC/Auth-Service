ac8a9e6ddd1f00ac81a685a6af02b2d30ee3b94b
christopher k <30932534+EleisonC@users.noreply.github.com>
2024-06-16 22:44:03 +0300

    Revert "Implement Redis"

--- auth-service/src/services/data_stores/mod.rs
+++ auth-service/src/services/data_stores/mod.rs
@@ -3,8 +3,6 @@ pub mod hashset_banned_token;
 pub mod hashmap_two_fa_code_store;
 pub mod mock_email_client;
 pub mod postgres_user_store;
-pub mod redis_banned_token_store;
-pub mod redis_two_fa_code_store;
 
 
 pub use hashmap_user_store::*;
@@ -12,5 +10,3 @@ pub use hashset_banned_token::*;
 pub use hashmap_two_fa_code_store::*;
 pub use mock_email_client::*;
 pub use postgres_user_store::*;
-pub use redis_banned_token_store::*;
-pub use redis_two_fa_code_store::*;
